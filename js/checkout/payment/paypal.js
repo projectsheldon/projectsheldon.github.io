cb843ed65d9d49d99a3321c75fe4b47e593caed2
Penguu <penguusoftware@gmail.com>
2026-01-07 10:29:17 +0200

    Update paypal.js

--- js/checkout/payment/paypal.js
+++ js/checkout/payment/paypal.js
@@ -6,17 +6,17 @@ paypal.Buttons({
     createOrder: async () => {
         const type = new URLSearchParams(window.location.search).get("type") || "";
 
-        const createRes = await fetch(`${API_URL}sheldon/paypal/create/${type}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" }
+        const createRes = await fetch(`${API_URL}sheldon/paypal/create`, {
+            body: {
+                productId: type,
+                discordId: "0"
+            }
         });
         const createData = await createRes.json();
         return createData.id;
     },
     onApprove: async (data, actions) => {
         const captureRes = await fetch(`${API_URL}sheldon/paypal/capture`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ id: data.orderID })
         });
 
