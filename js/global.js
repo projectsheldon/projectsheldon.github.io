53cbf4783ff53f9b17395079dd2b42927a1e2c09
Penguu <penguusoftware@gmail.com>
2026-01-07 14:36:05 +0200

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:3350/";
-// export const API_URL = "https://5.249.161.40:3350/";
+// export const API_URL = "http://localhost:3350/";
+export const API_URL = "https://5.249.161.40:3350/";
 
 export async function GetProducts() {
     try {
